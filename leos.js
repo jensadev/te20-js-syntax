dfc8ff9dd1bce5865b0c1b11fbe20b9f72b0baee
jensnti <jens.andreasson@ntig.se>
2022-09-20 13:05:52 +0200

    ✨ leos hypotenusa

---
+++ leos.js
@@ -0,0 +1,12 @@
+// import javax.swing.*;
+
+// public class pythagoras{
+// 	public static void main (String[] arg) {
+// 	double a = Double.parseDouble (JOptionPane.showInputDialog("F\u00F6rsta sidan i triangeln"));
+// 	double b = Double.parseDouble (JOptionPane.showInputDialog("Andra sidan i triangeln"));
+// 	double c = Math.sqrt(a*a + b*b);
+// 	JOptionPane.showMessageDialog (null, "Hypotenusans l\u00E4ngd \u00E4r " + c);
+// 	}
+// }
+
+// ni behöver 2 inmatningar och sedan en uträkning och en utskrift
